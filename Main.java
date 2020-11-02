75ff80794125503f3621912d99776112af71a641
qurek9038 <qurek9038@wrdsb.ca>
2020-11-02 07:17:14 +0000

    Completed Array Test

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program gives a grade for a five question multiple choice test
+ * @author Khazina Qureshi
  */
 public class Main {
 
@@ -9,7 +10,43 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //Create the scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Create the array for 5 test answers to be stored
+    String[] tAnswers = new String[5];  
+
+    //Ask the user to enter the answers to the test
+    System.out.println("please enter the answers to the test:");
+
+    //Initialize and ensure the user enters an answer that is greater than 0 
+    for(int i = 0; i< tAnswers.length; i++){
+      tAnswers[i] = input.nextLine();
+    }
+
+    //Create another array for 5 student test answers to be stored
+    String[] sAnswers = new String[5];
+
+    //Ask the user to enter the students answers
+    System.out.println("Please enter the student answers:");
+
+    //Initialize and ensure the user enters an answer that is greater than 0
+   for(int i = 0; i< sAnswers.length; i++){
+     sAnswers[i] = input.nextLine();
+   }
+
+    //Declare a variable for the students final score
+    int score = 0;
+
+    //Compare the two arrays and add one to score for each right answer
+    for(int i=0; i< tAnswers.length; i++){
+      if(tAnswers[i].equals(sAnswers[i])){
+        score +=1;
+      }
+    }
+
+    //Display the score to user
+    System.out.println("Score:" + score);
+
   }
 }
